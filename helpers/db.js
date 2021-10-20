2f922d89c11e2720f21de9a4b176dd6cd8d23772
koindra-nss <92796535+koindra-nss@users.noreply.github.com>
2021-10-20 20:01:15 +0700

    Update db.js

--- helpers/db.js
+++ helpers/db.js
@@ -2,7 +2,7 @@ const { query } = require('express-validator');
 const { Client } = require('pg');
 
 const client = new Client({
-  connectionString: "postgres://kttwourrbkwrcb:1f0c903d312ded5b94fba44abaead4b3c70f9fbc3a756ff9e7b9afa65cc74aa9@ec2-34-233-64-238.compute-1.amazonaws.com:5432/d3rf92mldag7hn",//process.env.DATABASE_URL,
+  connectionString: process.env.DATABASE_URL,
   ssl: {
     rejectUnauthorized: false
   }
